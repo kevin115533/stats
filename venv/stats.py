3b0010d3ab0c40dde54882c6b5cc31d9938f3d99
Kevin Tran <kevin115533@yahoo.com>
2019-10-17 13:47:03 -0600

    weekly labs
    defining functions

--- venv/stats.py
+++ venv/stats.py
@@ -3,21 +3,29 @@ File: stats.py
 
 This programs finds the median, mode, and mean of a set of numbers
 """
+import statistics
 
 def main():
-    sum1 = 0.0
     count = 0
     number = input("Enter a number or press Enter to quit: ")
+    numberList = []
     while number != "":
         count += 1
         number = float(number)
-        sum1 += number
+        numberList = numberList + [number]
         number = input("Enter a number or press Enter to quit: ")
-    average = mean(sum1, count)
+    average = mean(numberList, count)
     print("The average number is", round(average,2))
+    print("The median number is", round(median(numberList),2))
+    print("The mode of list of numbers is ", mode(numberList))
 
 def mean(x,y):
-    return x / y
+    return sum(x) / y
 
+def median(x):
+    return statistics.median(x)
+
+def mode(x):
+    return statistics.mode(x)
 
 main()
